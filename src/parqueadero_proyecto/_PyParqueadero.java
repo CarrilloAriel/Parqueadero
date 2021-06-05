f4c72b64f7eb0d64e93b4510ec6fced57918e8a7
MishellCifuentes <62954634+MishellCifuentes@users.noreply.github.com>
2021-06-04 21:13:38 -0500

    Se modifico en la 38

--- src/parqueadero_proyecto/_PyParqueadero.java
+++ src/parqueadero_proyecto/_PyParqueadero.java
@@ -34,7 +34,7 @@ public class _PyParqueadero {
        ColaDeCarros.insertar(new Vehiculo("1", "2023", new Persona("Pepe", "1")));
        ColaDeCarros.insertar(new Vehiculo("2", "2023", new Persona("Pepe", "1")));
        ColaDeCarros.insertar(new Vehiculo("3", "2023", new Persona("Pepe", "1")));
-       //mishell
+        System.out.println("holaaaaa");
         //System.out.println("Atendido"+ColaDeCarros.atender());
        if(Parqueaderos.hayDisponible()){
            Parqueaderos.imprimir();
