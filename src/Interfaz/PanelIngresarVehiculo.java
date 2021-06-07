6430973a5639a24a148c902125a67afca9607a3e
Dani <Dani@DANI>
2021-06-06 19:45:31 -0500

    Clase administracion

--- src/Interfaz/PanelIngresarVehiculo.java
+++ src/Interfaz/PanelIngresarVehiculo.java
@@ -5,7 +5,7 @@
  */
 package Interfaz;
 
-
+/*
 import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
@@ -19,7 +19,7 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.border.Border;
 import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.element.Text;*/
 import com.sun.javafx.geom.Rectangle;
 
 import java.io.File;
